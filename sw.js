511fa1583387a8f34607f3817f67bced8d08f37b
samuelgavelli-create <samuel.gavelli@gmail.com>
2026-08-27 00:07:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* IL CONTO - service worker offline
+   Cambia CACHE quando aggiorni index.html (es. conto-v5-2) */
+const CACHE = 'conto-v5-1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Rete se c'è, altrimenti cache: così vedi subito le versioni nuove ma funzioni offline
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return r;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
